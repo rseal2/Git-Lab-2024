8d98af3daee9c780db01234a077c19a5ba06c24a
rseal2 <rseal2@huskers.unl.edu>
2024-02-07 11:42:53 -0600

    finished lab

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -3,7 +3,8 @@ package edu.unl.raikes.git;
 public class GitLab {
     
     public static void main(String[] args) { 
-        System.out.println("This is a lesson on git");
+        GitLabHelper helper = new GitLabHelper(new String[] {"Name1", "Name2"});
+        helper.PrintWelcomeMessage();
     } 
     
 }
---
+++ src/edu/unl/raikes/git/GitLabHelper.java
@@ -0,0 +1,17 @@
+package edu.unl.raikes.git;
+
+import java.util.Arrays;
+
+public class GitLabHelper {
+    
+    private String[] persons; 
+    
+    GitLabHelper(String[] persons) { 
+        this.persons = persons; 
+    }
+    
+    public void PrintWelcomeMessage() { 
+        System.out.printf("Git lab by: " + Arrays.toString(persons));
+    }
+
+}
